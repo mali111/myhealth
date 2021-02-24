5d5ec61bed87ce44f7f08e49e8aac460d594a295
Ali Malik <muhammad.malik@cgi.com>
2021-02-24 20:26:11 +0100

    Carousel Implementation and Medication Data implementation

--- src/main/java/com/myhealth/wedigitalize/controller/OverviewController.java
+++ src/main/java/com/myhealth/wedigitalize/controller/OverviewController.java
@@ -1,5 +1,7 @@
 package com.myhealth.wedigitalize.controller;
 
+import com.myhealth.wedigitalize.medication.Medication;
+import com.myhealth.wedigitalize.medication.repository.MedicationRepository;
 import com.myhealth.wedigitalize.patient.Patient;
 import com.myhealth.wedigitalize.patient.repository.PatientRepository;
 import java.util.Map;
@@ -9,24 +11,35 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class OverviewController {
- @Autowired
- PatientRepository patientRepository;
+  @Autowired PatientRepository patientRepository;
+  @Autowired MedicationRepository medicationRepository;
 
-@GetMapping("overview")
-public String getOverview(Map<String ,Object> model) {
-  Patient patient = patientRepository.findById(2);
-  model.put("firstName", patient.getFirstName().toString());
-  model.put("lastName", patient.getLastName().toString());
-  model.put("dob", patient.getDob().toString());
-  model.put("age", patient.getAge().toString());
-  model.put("gender", patient.getGender().toString());
-  model.put("bloodGroup", patient.getBloodGroup().toString());
-  model.put("weight", patient.getWeight().toString());
-  model.put("organDonor", patient.isOrganDonor());
-  model.put("pregnant", patient.isPregnant());
-  model.put("smoker", patient.isSmoker());
-  model.put("insurance", patient.getInsurance());
-  model.put("additionalInfo", patient.getAdditionalInfo());
-  return "overview";
+  @GetMapping("overview")
+  public String getOverview(Map<String, Object> model) {
+    Patient patient = patientRepository.findById(2);
+    model.put("firstName", patient.getFirstName().toString());
+    model.put("lastName", patient.getLastName().toString());
+    model.put("dob", patient.getDob().toString());
+    model.put("age", patient.getAge().toString());
+    model.put("gender", patient.getGender().toString());
+    model.put("bloodGroup", patient.getBloodGroup().toString());
+    model.put("weight", patient.getWeight().toString());
+    model.put("organDonor", patient.isOrganDonor());
+    model.put("pregnant", patient.isPregnant());
+    model.put("smoker", patient.isSmoker());
+    model.put("insurance", patient.getInsurance());
+    model.put("additionalInfo", patient.getAdditionalInfo());
+
+    Medication medication = medicationRepository.findByPznNumber(0631340);
+    model.put("medicationName", medication.getMedicationName().toString());
+    model.put("medicationPZN", medication.getPznNumber());
+    model.put("medicationDosage", medication.getDosage().toString());
+    model.put("medicationStartDate", medication.getStartDate().toString());
+    model.put("medicationEndDate", medication.getEndDate().toString());
+    model.put("medicationActiveIngredient", medication.getActiveIngredient().toString());
+    model.put("medicationNotes", medication.getNotes().toString());
+    model.put("medicationActive", medication.isActivate());
+    model.put("medicationPrescriptionRequired", medication.isPrescriptionRequired());
+    return "overview";
   }
 }
