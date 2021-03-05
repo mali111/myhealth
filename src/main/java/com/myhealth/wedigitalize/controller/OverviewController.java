7bad7bae3beaff73da882b016e219b3db794a158
Ali Malik <muhammad.malik@cgi.com>
2021-03-05 15:20:26 +0100

    EmergencyPage Added

--- src/main/java/com/myhealth/wedigitalize/controller/OverviewController.java
+++ src/main/java/com/myhealth/wedigitalize/controller/OverviewController.java
@@ -71,4 +71,40 @@ public class OverviewController {
 
     return "overview";
   }
+
+  @GetMapping("firstAidOverview")
+  public String firstAidOverview(
+      Map<String, Object> model,
+      Model drugInt,
+      Model contact,
+      Model vaccine,
+      Model allergy,
+      Model previousIllness) {
+    // General Patient Data
+    Patient patient = patientRepository.findById(1);
+    model.put("firstName", patient.getFirstName().toString());
+    model.put("lastName", patient.getLastName().toString());
+    model.put("dob", patient.getDob().toString());
+    model.put("age", patient.getAge().toString());
+    model.put("gender", patient.getGender().toString());
+    model.put("bloodGroup", patient.getBloodGroup().toString());
+    model.put("weight", patient.getWeight().toString());
+    model.put("organDonor", patient.isOrganDonor());
+    model.put("pregnant", patient.isPregnant());
+    model.put("smoker", patient.isSmoker());
+    model.put("insurance", patient.getInsurance());
+    model.put("additionalInfo", patient.getAdditionalInfo());
+    // drug Intollerance List
+    drugInt.addAttribute("drugIntollerances", drugIntolleranceRepository.findAll());
+    // contacts list
+    contact.addAttribute("contacts", contactRepository.findAll());
+    // vaccine list
+    vaccine.addAttribute("vaccines", vaccineRepository.findAll());
+    // Allergy List
+    allergy.addAttribute("allergies", allergyRepository.findAll());
+    // Pervious Illness List
+    previousIllness.addAttribute("previousIllnesses", previousIllnessesRepository.findAll());
+
+    return "firstAidOverview";
+  }
 }
