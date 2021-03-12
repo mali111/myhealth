0f16add17734e34748b1375ebcdf197fad7d0376
Ali Malik <muhammad.malik@cgi.com>
2021-03-12 09:44:01 +0100

    WIP - Registration added

--- src/main/java/com/myhealth/wedigitalize/controller/OverviewController.java
+++ src/main/java/com/myhealth/wedigitalize/controller/OverviewController.java
@@ -9,10 +9,16 @@ import com.myhealth.wedigitalize.patient.repository.PatientRepository;
 import com.myhealth.wedigitalize.patient.repository.PreviousIllnessesRepository;
 import com.myhealth.wedigitalize.patient.repository.VaccineRepository;
 import java.util.Map;
+import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class OverviewController {
@@ -97,5 +103,33 @@ public class OverviewController {
     previousIllness.addAttribute("previousIllnesses", previousIllnessesRepository.findAll());
 
     return "firstAidOverview";
+  }
+
+  @RequestMapping(value = "/registration", method = RequestMethod.POST)
+  public String submit(
+      @Valid @ModelAttribute("patient") Patient patient, BindingResult result, ModelMap model) {
+    if (result.hasErrors()) {
+      return "error";
+    }
+    model.addAttribute("firstname", patient.getFirstName());
+    model.addAttribute("lastname", patient.getLastName());
+    model.addAttribute("dob", patient.getDob().toString());
+    model.addAttribute("age", patient.getAge().toString());
+    model.addAttribute("gender", patient.getGender().toString());
+    model.addAttribute("bloodGroup", patient.getBloodGroup().toString());
+    model.addAttribute("weight", patient.getWeight().toString());
+    model.addAttribute("organDonor", patient.isOrganDonor());
+    model.addAttribute("pregnant", patient.isPregnant());
+    model.addAttribute("smoker", patient.isSmoker());
+    model.addAttribute("insurance", patient.getInsurance());
+    model.addAttribute("additionalInfo", patient.getAdditionalInfo());
+    patientRepository.save(patient);
+    return "registration";
+  }
+
+  @RequestMapping(value = "/registration", method = RequestMethod.GET)
+  public String showPatient(Model model) {
+    model.addAttribute("patient", new Patient());
+    return "registration";
   }
 }
